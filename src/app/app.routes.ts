b84fca1480ee5e6d4def6d66b39117e88e08025c
STARS1LVER <diegocardenaz11@gmail.com>
2024-02-19 23:38:52 -0500

    Adding footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -24,7 +24,7 @@ export const routes: Routes = [
       },
       {
         path: '',
-        redirectTo: 'categories-food',
+        redirectTo: 'home',
         pathMatch: 'full'
       }
     ]
